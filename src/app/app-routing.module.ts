dcdcdd3c21a7af9785383a3fb5e4d38c7bced00e
fabiomedeiros <fmauff1978@gmail.com>
2024-05-02 11:47:15 -0300

    antes de apagar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact/contact.component';
+import { ArticlesComponent } from './articles/articles/articles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: 'contact', component: ContactComponent },
+  { path: 'articles', component: ArticlesComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'articles' },
+
+  { path: 'posts', loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule) },
+  { path: '**', redirectTo: 'articles' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ContactComponent } from './contact/contact/contact.component';
+import { ArticlesComponent } from './articles/articles/articles.component';
+import { ScullyLibModule } from '@scullyio/ng-lib';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent,
+    ArticlesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ScullyLibModule
   ],
   providers: [],
   bootstrap: [AppComponent]
